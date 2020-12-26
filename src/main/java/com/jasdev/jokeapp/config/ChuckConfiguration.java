b7d9030f74878faeb8de653beb28abc36dc4620d
jasdevsidhu12 <jasdev.project.12@gmail.com>
2020-12-26 13:19:20 +0000

    Initial Joke App with xml configurations

--- src/main/java/com/jasdev/jokeapp/config/ChuckConfiguration.java
+++ src/main/java/com/jasdev/jokeapp/config/ChuckConfiguration.java
@@ -1,14 +1,14 @@
-package com.jasdev.jokeapp.config;
-
-import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ChuckConfiguration {
-
-    @Bean
-    public ChuckNorrisQuotes chuckNorrisQuotes() {
-        return new ChuckNorrisQuotes();
-    }
-}
+//package com.jasdev.jokeapp.config;
+//
+//import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ChuckConfiguration {
+//
+//    @Bean
+//    public ChuckNorrisQuotes chuckNorrisQuotes() {
+//        return new ChuckNorrisQuotes();
+//    }
+//}
